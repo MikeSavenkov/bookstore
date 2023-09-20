985dcc502cfe6675cc650ae91dd1fa8cf6db9d49
Mike <savenkovmike@gmail.com>
2023-09-20 23:06:48 +0300

    Add liquibase changelogs, pg property

--- src/main/java/com/msavenkov/bookstore/BookstoreApplication.java
+++ src/main/java/com/msavenkov/bookstore/BookstoreApplication.java
@@ -3,8 +3,10 @@ package com.msavenkov.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan("com.msavenkov.bookstore.repository")
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
